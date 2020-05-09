32a8ec8e3df14178ccc328fb124d57a83debe7da
scb4377 <scblevins27@gmail.com>
2020-05-09 19:16:35 -0400

    added no zoom and fullScreen capabilities

--- sound.js
+++ sound.js
@@ -3,6 +3,46 @@ window.onload = function () {
   const canvas = document.getElementById('canvas');
   const h3 = document.getElementById('name');
   const audio = document.getElementById('audio');
+  /* Get the documentElement (<html>) to display the page in fullscreen */
+  var elem = document.documentElement;
+
+  /* View in fullscreen */
+  function openFullscreen() {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+      /* Firefox */
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+      /* Chrome, Safari and Opera */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+      /* IE/Edge */
+      elem.msRequestFullscreen();
+    }
+  }
+
+  /* Close fullscreen */
+  function closeFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      /* Firefox */
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      /* Chrome, Safari and Opera */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      /* IE/Edge */
+      document.msExitFullscreen();
+    }
+  }
+  document
+    .getElementById('fullScreen')
+    .addEventListener('click', openFullscreen);
+  document
+    .getElementById('exitFullScreen')
+    .addEventListener('click', closeFullscreen);
 
   file.onchange = function () {
     const files = this.files;
