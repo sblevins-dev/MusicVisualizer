026be6cb137f9d6c311fb94e9e92c01b50b8f362
scb4377 <scblevins27@gmail.com>
2020-05-09 16:53:18 -0400

    added color and transitions

--- sound.js
+++ sound.js
@@ -39,11 +39,12 @@ window.onload = function () {
 
     let barHeight;
     let x = 0;
+    let angle = 2;
+    let y;
+    let test;
 
     function renderFrame() {
       requestAnimationFrame(renderFrame);
-
-      x = 0;
 
       analyser.getByteFrequencyData(dataArray);
 
@@ -53,6 +54,12 @@ window.onload = function () {
       let r, g, b;
       let bars = 360;
       let barWidth = 2;
+
+      // if
+      //angle -= 0.01;
+      // ctx.rotate(45 * Math.PI) / 180;
+      // console.log(angle);
+      // let rads = (angle * Math.PI) / bars;
 
       let center_x = canvas.width / 2;
       let center_y = canvas.height / 2;
@@ -73,32 +80,17 @@ window.onload = function () {
       canvas.height = window.innerHeight;
       //   const ctx = canvas.getContext('2d');
 
-      ctx.beginPath();
-      ctx.arc(center_x, center_y, radius, 0, 2 * Math.PI);
-      ctx.stroke();
+      // ctx.beginPath();
+      // ctx.arc(center_x, center_y, radius, 0, 2 * Math.PI);
+      // ctx.rotate(45 * Math.PI) / 180;
+      // ctx.stroke();
 
-      //   function reportWindowSize() {
-      //     var gradient = ctx.createLinearGradient(0, 50, 0, canvas.height);
-      //     gradient.addColorStop(0, `rgba(0, 0, 1, 1)`);
-      //     gradient.addColorStop(1, 'rgba(0, 0, 51, 1)');
-      //     ctx.fillStyle = gradient;
-      //     ctx.fillRect(0, 0, WIDTH, HEIGHT);
-      //     let center_x = WIDTH / 2;
-      //     let center_y = HEIGHT / 2;
-      //     let radius = 150;
-      //     ctx.beginPath();
-      //     ctx.arc(center_x, center_y, radius, 0, 2 * Math.PI);
-      //     ctx.stroke();
-      //   }
-
-      //   window.addEventListener('resize', reportWindowSize);
-      let maxFr = Math.max(dataArray);
-      let avgFr = Math.min(dataArray);
+      let test2 = 0;
       for (let i = 0; i < bars; i++) {
         //barHeight = dataArray[i] * 2.5;
 
-        let rads = (Math.PI * 2) / bars;
-        barHeight = dataArray[i] * 0.9;
+        // let rads = (Math.PI * 2) / bars;
+        barHeight = dataArray[i] * 1;
 
         //trying to figure out base
         if (dataArray[i] > 300) {
@@ -121,34 +113,42 @@ window.onload = function () {
           r = 204;
           g = 255;
           b = 50;
+          barHeight = dataArray[i] * 0.9;
         } else if (dataArray[i] > 170) {
           r = 0;
           g = 50;
           b = 255;
+          barHeight = dataArray[i] * 0.9;
         } else if (dataArray[i] > 150) {
           r = 0;
           g = 100;
           b = 255;
+          barHeight = dataArray[i] * 0.9;
         } else if (dataArray[i] > 130) {
           r = 0;
           g = 200;
           b = 255;
+          barHeight = dataArray[i] * 0.8;
         } else if (dataArray[i] > 110) {
           r = 0;
           g = 255;
           b = 200;
+          barHeight = dataArray[i] * 0.8;
         } else if (dataArray[i] > 90) {
           r = 0;
           g = 255;
           b = 100;
+          barHeight = dataArray[i] * 0.8;
         } else if (dataArray[i] > 75) {
           r = 0;
           g = 255;
           b = 50;
+          barHeight = dataArray[i] * 0.8;
         } else if (dataArray[i] > 50) {
           r = 0;
           g = 255;
           b = 0;
+          barHeight = dataArray[i] * 0.8;
         } else if (dataArray[i] > 25) {
           r = 0;
           g = 100;
@@ -161,24 +161,32 @@ window.onload = function () {
         //ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
         //ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
 
+        let rads = (angle * Math.PI) / bars;
         x = center_x + Math.cos(rads * i) * radius;
         y = center_y + Math.sin(rads * i) * radius;
         let x_end = center_x + Math.cos(rads * i) * (radius + barHeight);
         let y_end = center_y + Math.sin(rads * i) * (radius + barHeight);
         //draw a bar
+        //let center_x = canvas.width / 2;
+        //let center_y = canvas.height / 2;
+        // rads = (angle or 2 * Math.PI) / bars or 360
+        // let angle2 = 0;
+        // angle2 += 0.001;
+
         drawBar(x, y, x_end, y_end, barWidth, dataArray[i]);
 
         function drawBar(x1, y1, x2, y2, width, frequency) {
           var lineColor = `rgb(${r}, ${g}, ${b})`;
           //'rgb(' + frequency + ', ' + frequency + ', ' + 205 + ')';
-
           ctx.strokeStyle = lineColor;
           ctx.lineWidth = width;
           ctx.beginPath();
           ctx.moveTo(x1, y1);
           ctx.lineTo(x2, y2);
           ctx.stroke();
+          ctx.fill();
         }
+
         //x += barWidth + 10;
       }
     }
