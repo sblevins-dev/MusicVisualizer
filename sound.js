f9054be2829724eeb3183fdf8a56108a29c68505
Stephen Blevins <scblevins27@gmail.com>
2021-12-13 12:51:55 -0500

    bugs

--- sound.js
+++ sound.js
@@ -1,253 +1,238 @@
+const file = document.getElementById("file-input");
+const canvas = document.getElementById("canvas");
+const h3 = document.getElementById("name");
+const audio = document.getElementById("audio");
+const demo = document.querySelector(".demo");
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
 
-  const file = document.getElementById("file-input");
-  const canvas = document.getElementById("canvas");
-  const h3 = document.getElementById("name");
-  const audio = document.getElementById("audio");
-  const demo = document.querySelector(".demo");
-  /* Get the documentElement (<html>) to display the page in fullscreen */
-  var elem = document.documentElement;
+// Create context
+const ctx = canvas.getContext("2d");
 
-  // Create context
-  const ctx = canvas.getContext("2d");
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) {
+    /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
 
-  /* View in fullscreen */
-  function openFullscreen() {
-    if (elem.requestFullscreen) {
-      elem.requestFullscreen();
-    } else if (elem.mozRequestFullScreen) {
-      /* Firefox */
-      elem.mozRequestFullScreen();
-    } else if (elem.webkitRequestFullscreen) {
-      /* Chrome, Safari and Opera */
-      elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) {
-      /* IE/Edge */
-      elem.msRequestFullscreen();
-    }
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    /* IE/Edge */
+    document.msExitFullscreen();
+  }
+}
+document.getElementById("fullScreen").addEventListener("click", openFullscreen);
+document
+  .getElementById("exitFullScreen")
+  .addEventListener("click", closeFullscreen);
+
+const context = new AudioContext(); //|| context.AudioContext;
+let src = context.createMediaElementSource(audio);
+const analyser = context.createAnalyser();
+
+let musicSource = [
+  "./music/blueMonday.mp3",
+  "./music/onYourMind.mp3",
+  "./music/saveYourTears.mp3",
+  "./music/desire.mp3",
+];
+
+let index = 1;
+let next = document.getElementById("next");
+next.addEventListener("click", () => {
+  let flag = null;
+  visualize(musicSource, flag, index);
+  if (index == 3) {
+    index = 0;
+  } else {
+    index++;
+  }
+});
+
+function playSongs(file, c) {
+  audio.src = file[c];
+}
+function visualize(file, flag, index) {
+  let name;
+  let songNames = ["Blue Monday", "On Your Mind", "Save Your Tears", "Desire"];
+  let files;
+  if (flag == "fileChoice") {
+    files = file.files;
+    audio.src = URL.createObjectURL(files[0]);
+    name = files[0].name;
+  } else {
+    playSongs(file, index);
+    name = songNames[index];
   }
 
-  /* Close fullscreen */
-  function closeFullscreen() {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-      /* Firefox */
-      document.mozCancelFullScreen();
-    } else if (document.webkitExitFullscreen) {
-      /* Chrome, Safari and Opera */
-      document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) {
-      /* IE/Edge */
-      document.msExitFullscreen();
-    }
-  }
-  document
-    .getElementById("fullScreen")
-    .addEventListener("click", openFullscreen);
-  document
-    .getElementById("exitFullScreen")
-    .addEventListener("click", closeFullscreen);
+  h3.innerText = `${name}`;
 
-  const context = new AudioContext() //|| context.AudioContext;
-  let src = context.createMediaElementSource(audio);
-  const analyser = context.createAnalyser();
+  //possibly for bass
+  const bassFilter = context.createBiquadFilter();
+  bassFilter.type = "lowshelf";
 
-  let musicSource = [
-    "./music/blueMonday.mp3",
-    "./music/onYourMind.mp3",
-    "./music/saveYourTears.mp3",
-    "./music/desire.mp3",
-  ];
+  src.connect(analyser);
+  analyser.connect(context.destination);
 
-  let index = 1;
-  let next = document.getElementById("next");
-  next.addEventListener("click", () => {
-    let flag = null;
-    visualize(musicSource, flag, index);
-    if (index == 3) {
-      index = 0;
+  analyser.fftSize = 4096;
+
+  const bufferLength = analyser.frequencyBinCount;
+
+  const dataArray = new Uint8Array(bufferLength);
+
+  let barHeight;
+  let x = 0;
+  let angle = 2;
+  let y;
+
+  function renderFrame() {
+    requestAnimationFrame(renderFrame);
+
+    analyser.getByteFrequencyData(dataArray);
+
+    let r, g, b;
+    let bars = 360;
+    let barWidth = 5;
+
+    let center_x = canvas.width / 2;
+    let center_y = canvas.height / 2;
+    let radius;
+    if (canvas.width < 700 || canvas.height < 500) {
+      radius = 40;
     } else {
-      index++;
-    }
-  });
-
-  function playSongs(file, c) {
-    audio.src = file[c];
-    name = "On Your Mind by Kaskade";
-  }
-  function visualize(file, flag, index) {
-    let name;
-    let songNames = [
-      "Blue Monday",
-      "On Your Mind",
-      "Save Your Tears",
-      "Desire",
-    ];
-    let files;
-    if (flag == "fileChoice") {
-      files = file.files;
-      audio.src = URL.createObjectURL(files[0]);
-      name = files[0].name;
-    } else {
-      playSongs(file, index);
-      name = songNames[index];
+      radius = 150;
     }
 
-    h3.innerText = `${name}`;
+    var gradient = ctx.createLinearGradient(0, 50, 0, canvas.height);
+    gradient.addColorStop(0, "rgba(0, 0, 1, 1)");
+    gradient.addColorStop(1, "rgba(0, 0, 51, 1)");
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    //possibly for bass
-    const bassFilter = context.createBiquadFilter();
-    bassFilter.type = "lowshelf";
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-    src.connect(analyser);
-    analyser.connect(context.destination);
-
-    analyser.fftSize = 4096;
-
-    const bufferLength = analyser.frequencyBinCount;
-
-    const dataArray = new Uint8Array(bufferLength);
-
-    const WIDTH = canvas.width;
-    const HEIGHT = canvas.height;
-
-    const barWidth = (WIDTH / bufferLength) * 13;
-
-    let barHeight;
-    let x = 0;
-    let angle = 2;
-    let y;
-
-    function renderFrame() {
-      requestAnimationFrame(renderFrame);
-
-      analyser.getByteFrequencyData(dataArray);
-
-      let r, g, b;
-      let bars = 360;
-      let barWidth = 5;
-
-      let center_x = canvas.width / 2;
-      let center_y = canvas.height / 2;
-      let radius;
-      if (canvas.width < 700 || canvas.height < 500) {
-        radius = 40;
+    let test2 = 0;
+    for (let i = 0; i < bars; i++) {
+      if (canvas.width < 700 || canvas.height < 400) {
+        barHeight = dataArray[i] * 0.3;
       } else {
-        radius = 150;
+        barHeight = dataArray[i] * 0.8;
       }
 
-      var gradient = ctx.createLinearGradient(0, 50, 0, canvas.height);
-      gradient.addColorStop(0, "rgba(0, 0, 1, 1)");
-      gradient.addColorStop(1, "rgba(0, 0, 51, 1)");
-      ctx.fillStyle = gradient;
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      //trying to figure out base
+      if (dataArray[i] > 300) {
+        r = 255;
+        g = 5;
+        b = 0;
+      } else if (dataArray[i] > 235) {
+        r = 250;
+        g = 5;
+        b = 100;
+      } else if (dataArray[i] > 210) {
+        r = 250;
+        g = 0;
+        b = 250;
+      } else if (dataArray[i] > 200) {
+        r = 250;
+        g = 255;
+        b = 0;
+      } else if (dataArray[i] > 180) {
+        r = 204;
+        g = 255;
+        b = 50;
+      } else if (dataArray[i] > 170) {
+        r = 0;
+        g = 50;
+        b = 255;
+      } else if (dataArray[i] > 150) {
+        r = 0;
+        g = 100;
+        b = 255;
+      } else if (dataArray[i] > 130) {
+        r = 0;
+        g = 200;
+        b = 255;
+      } else if (dataArray[i] > 110) {
+        r = 0;
+        g = 255;
+        b = 200;
+      } else if (dataArray[i] > 90) {
+        r = 0;
+        g = 255;
+        b = 100;
+      } else if (dataArray[i] > 75) {
+        r = 0;
+        g = 255;
+        b = 50;
+      } else if (dataArray[i] > 50) {
+        r = 0;
+        g = 255;
+        b = 0;
+      } else if (dataArray[i] > 25) {
+        r = 0;
+        g = 100;
+        b = 0;
+      } else {
+        r = 0;
+        g = 50;
+        b = 0;
+      }
 
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
+      let rads = (angle * Math.PI) / bars;
+      x = center_x + Math.cos(rads * i) * radius;
+      y = center_y + Math.sin(rads * i) * radius;
+      let x_end = center_x + Math.cos(rads * i) * (radius + barHeight);
+      let y_end = center_y + Math.sin(rads * i) * (radius + barHeight);
 
-      let test2 = 0;
-      for (let i = 0; i < bars; i++) {
-        if (canvas.width < 700 || canvas.height < 400) {
-          barHeight = dataArray[i] * 0.3;
-        } else {
-          barHeight = dataArray[i] * 0.8;
-        }
+      drawBar(x, y, x_end, y_end, barWidth, dataArray[i]);
 
-        //trying to figure out base
-        if (dataArray[i] > 300) {
-          r = 255;
-          g = 5;
-          b = 0;
-        } else if (dataArray[i] > 235) {
-          r = 250;
-          g = 5;
-          b = 100;
-        } else if (dataArray[i] > 210) {
-          r = 250;
-          g = 0;
-          b = 250;
-        } else if (dataArray[i] > 200) {
-          r = 250;
-          g = 255;
-          b = 0;
-        } else if (dataArray[i] > 180) {
-          r = 204;
-          g = 255;
-          b = 50;
-        } else if (dataArray[i] > 170) {
-          r = 0;
-          g = 50;
-          b = 255;
-        } else if (dataArray[i] > 150) {
-          r = 0;
-          g = 100;
-          b = 255;
-        } else if (dataArray[i] > 130) {
-          r = 0;
-          g = 200;
-          b = 255;
-        } else if (dataArray[i] > 110) {
-          r = 0;
-          g = 255;
-          b = 200;
-        } else if (dataArray[i] > 90) {
-          r = 0;
-          g = 255;
-          b = 100;
-        } else if (dataArray[i] > 75) {
-          r = 0;
-          g = 255;
-          b = 50;
-        } else if (dataArray[i] > 50) {
-          r = 0;
-          g = 255;
-          b = 0;
-        } else if (dataArray[i] > 25) {
-          r = 0;
-          g = 100;
-          b = 0;
-        } else {
-          r = 0;
-          g = 50;
-          b = 0;
-        }
-
-        let rads = (angle * Math.PI) / bars;
-        x = center_x + Math.cos(rads * i) * radius;
-        y = center_y + Math.sin(rads * i) * radius;
-        let x_end = center_x + Math.cos(rads * i) * (radius + barHeight);
-        let y_end = center_y + Math.sin(rads * i) * (radius + barHeight);
-
-        drawBar(x, y, x_end, y_end, barWidth, dataArray[i]);
-
-        function drawBar(x1, y1, x2, y2, width, frequency) {
-          var lineColor = `rgb(${r}, ${g}, ${b})`;
-          ctx.strokeStyle = lineColor;
-          ctx.lineWidth = width;
-          ctx.beginPath();
-          ctx.lineCap = "round";
-          ctx.moveTo(x1, y1);
-          ctx.lineTo(x2, y2);
-          ctx.stroke();
-          ctx.fill();
-        }
+      function drawBar(x1, y1, x2, y2, width, frequency) {
+        var lineColor = `rgb(${r}, ${g}, ${b})`;
+        ctx.strokeStyle = lineColor;
+        ctx.lineWidth = width;
+        ctx.beginPath();
+        ctx.lineCap = "round";
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.stroke();
+        ctx.fill();
       }
     }
-
-    audio.play();
-
-    renderFrame();
   }
 
-  file.onchange = () => {
-    let flag1 = "fileChoice";
-    context.resume();
-    visualize(file, flag1);
-  };
+  audio.play();
 
-  demo.onclick = () => {
-    // let src = ["./music/onYourMind.mp3", "./music/saveYourTears.mp3", "./music/desire.mp3"];
-    let flag2 = "demo";
-    context.resume();
-    visualize(musicSource, flag2, 0);
-  };
+  requestAnimationFrame(renderFrame());
+}
+
+file.onchange = () => {
+  let flag1 = "fileChoice";
+  context.resume();
+  visualize(file, flag1);
+};
+
+demo.onclick = () => {
+  let flag2 = "demo";
+  context.resume();
+  visualize(musicSource, flag2, 0);
+};
